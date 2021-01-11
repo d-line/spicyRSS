9d6d61ceba21ef80cb86060df90ed710dc715b22
Denis Kramchaninov <denis.kramchaninov@venafi.com>
2021-01-11 01:34:31 -0800

    logger, eslint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
+import log4js from 'log4js';
+
 dotenv.config();
+
+const logger = log4js.getLogger();
+logger.level = process.env.LOG_LEVEL;
 
 const app = express();
 const port = process.env.PORT;
@@ -9,4 +14,8 @@ app.get('/', (request, response) => {
   response.send('Hello world!');
 });
 
-app.listen(port, () => console.log(`Running on port ${port}`));
+logger.info('log4js log info');
+logger.debug('log4js log debug');
+logger.error('log4js log error');
+
+app.listen(port, () => logger.info(`Running on port ${port}`));
