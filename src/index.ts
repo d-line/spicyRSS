9d10bf0814f9313ff75bfb666aae12bf9b858d35
Denis Kramchaninov <denis.kramchaninov@venafi.com>
2021-01-11 09:14:44 -0800

    standard eslint config, routung controller, decorators

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,16 @@
-import express from 'express';
 import dotenv from 'dotenv';
 import log4js from 'log4js';
+import { createExpressServer } from 'routing-controllers';
+import { UserController } from './controller/user-controller';
 
 dotenv.config();
+const port = process.env.PORT;
 
 const logger = log4js.getLogger();
 logger.level = process.env.LOG_LEVEL;
 
-const app = express();
-const port = process.env.PORT;
-
-app.get('/', (request, response) => {
-  response.send('Hello world!');
+const app = createExpressServer({
+  controllers: [UserController]
 });
-
-logger.info('log4js log info');
-logger.debug('log4js log debug');
-logger.error('log4js log error');
 
 app.listen(port, () => logger.info(`Running on port ${port}`));
