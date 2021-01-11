646a04c73221c7f2108d9724d8bd6d10ad36c73f
Denis Kramchaninov <denis.kramchaninov@venafi.com>
2021-01-11 09:50:31 -0800

    middleware, interceptor, http context

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Get, Param } from 'routing-controllers';
+import { Action, Controller, Get, Param, UseAfter, UseBefore, UseInterceptor } from 'routing-controllers';
 import 'reflect-metadata';
+import { loggingAfter, loggingBefore } from '../middleware/middleware';
 
 @Controller()
+@UseBefore(loggingBefore)
+@UseAfter(loggingAfter)
+@UseInterceptor(function (action: Action, content: any) {
+  console.log('change response...');
+  content = `[${content}]`;
+  return content;
+})
 export class UserController {
   @Get('/users/:id')
   getOne (@Param('id') id: number) {
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import express, { Express } from 'express';
+import httpContext from 'express-http-context';
 import log4js from 'log4js';
-import { createExpressServer } from 'routing-controllers';
+import { useExpressServer } from 'routing-controllers';
 import { UserController } from './controller/user-controller';
 
 dotenv.config();
@@ -9,8 +12,17 @@ const port = process.env.PORT;
 const logger = log4js.getLogger();
 logger.level = process.env.LOG_LEVEL;
 
-const app = createExpressServer({
+const app: Express = express();
+app.use(bodyParser.json());
+app.use(httpContext.middleware);
+
+useExpressServer(app, {
   controllers: [UserController]
+});
+
+app.use((req, res, next) => {
+  httpContext.ns.bindEmitter(req);
+  httpContext.ns.bindEmitter(res);
 });
 
 app.listen(port, () => logger.info(`Running on port ${port}`));
