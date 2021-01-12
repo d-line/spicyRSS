b58ebe124e61ca184226f88ee5d251ee1d8c4bca
Denis Kramchaninov <denis.kramchaninov@venafi.com>
2021-01-11 17:07:24 -0800

    swagger, cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
-import express, { Express } from 'express';
+import express, { Express, RequestHandler } from 'express';
 import httpContext from 'express-http-context';
 import log4js from 'log4js';
 import { useExpressServer } from 'routing-controllers';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from '../src/swagger/openapi.json';
 import { UserController } from './controller/user-controller';
 import { GlobalErrorHandler } from './middleware/global-error-handler';
 
@@ -16,6 +19,8 @@ logger.level = process.env.LOG_LEVEL;
 const app: Express = express();
 app.use(bodyParser.json());
 app.use(httpContext.middleware);
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cors() as RequestHandler);
 
 useExpressServer(app, {
   controllers: [UserController],
