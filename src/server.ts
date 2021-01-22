f5b899cbfdef7ac38e847683232c8160524ca309
Denis Kramchaninov <denis.kramchaninov@venafi.com>
2021-01-21 17:32:57 -0800

    logger middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
-import * as express from 'express';
+import * as express from "express";
+
+function loggerMiddleware(request: express.Request, response: express.Response, next) {
+  console.log(`${request.method} ${request.path}`);
+  next();
+}
 
 const app = express();
+
+app.use(loggerMiddleware);
 
 app.get('/', (request, response) => {
   response.send('Hello, RSS!');
