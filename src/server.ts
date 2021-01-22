67cd5f1c3402c9ade64a81142d7a097e1947d37b
Denis Kramchaninov <denis.kramchaninov@venafi.com>
2021-01-21 17:40:08 -0800

    routing

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,17 @@ import * as bodyParser from 'body-parser';
 import * as express from "express";
 
 const app = express();
+const router = express.Router();
 
 app.use(bodyParser.json());
 
-app.post('/', (request, response) => {
+router.get('/', (request, response) => {
+  response.send('Spicy RSS');
+});
+
+router.post('/', (request, response) => {
   response.send(request.body);
 });
 
+app.use('/api', router);
 app.listen(5000);
