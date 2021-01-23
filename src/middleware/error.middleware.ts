7f6c14570dfd4250bcb4108c419873a1c0a767bc
Denis Kramchaninov <denis.kramchaninov@venafi.com>
2021-01-22 21:59:18 -0800

    fix lint

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,5 +1,6 @@
 import { ExpressErrorMiddlewareInterface, Middleware } from 'routing-controllers';
 
+// eslint-disable-next-line no-undef
 @Middleware({ type: 'after' })
 export class GlobalErrorHandler implements ExpressErrorMiddlewareInterface {
   error (error: any, request: any, response: any, next: () => any) {
