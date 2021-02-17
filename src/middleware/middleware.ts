5541bd0ce25c37fcdc0d6cf15eeca34bea4e85f4
D-Line <onlyredline+git@gmail.com>
2021-02-16 22:40:49 -0800

    wipe

--- src/middleware/middleware.ts
+++
@@ -1,9 +0,0 @@
-export function loggingBefore (request: any, response: any, next?: (err?: any) => any): any {
-  console.log('do something Before...');
-  next();
-}
-
-export function loggingAfter (request: any, response: any, next?: (err?: any) => any): any {
-  console.log('do something After...');
-  next();
-}
