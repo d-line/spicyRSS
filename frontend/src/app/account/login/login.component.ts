26e47c8cca6dd0de51c854ffc2bd760947ada078
D-Line <onlyredline+git@gmail.com>
2021-03-29 00:49:48 -0700

    styles for login page

--- frontend/src/app/account/login/login.component.ts
+++ frontend/src/app/account/login/login.component.ts
@@ -2,16 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
-
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { faArrowAltCircleRight } from '@fortawesome/free-regular-svg-icons';
 import { AccountService } from '../account.service';
 import { AlertService } from '../../alert/alert.service';
 
-@Component({ templateUrl: 'login.component.html' })
+@Component({ 
+  selector: 'app-login',
+  templateUrl: 'login.component.html',
+  styleUrls: ['./login.component.scss']
+})
 export class LoginComponent implements OnInit {
   form: FormGroup;
   loading = false;
   submitted = false;
   returnUrl: string;
+  faLock = faLock;
+  faArrowAltCircleRight = faArrowAltCircleRight;
 
   constructor(
     private formBuilder: FormBuilder,
